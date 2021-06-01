e769133fc781503adc4945a8e8f7d3af2ec02a86
smhrd <smhrd@222.102.104.167>
2021-06-01 10:32:29 +0900

    vscode 작업내용 이클립스 연동, controller단 추가 중

--- Rentue/src/sql.sql
+++ Rentue/src/sql.sql
@@ -2,22 +2,19 @@ create table rent_member(
 
 	rent_id varchar2(20),
 	ct_id varchar2(20),
-	rent_pw varchar2(20),
+	rent_pw varchar2(20) not null,
 	rent_name varchar2(20),
 	rent_num varchar2(20),
 	rent_addr varchar2(50)
-)
-select * from rent_member
+);
 
 create table ct_member(
 	
 	ct_id varchar2(20),
-	ct_pw varchar2(20),
+	ct_pw varchar2(20) not null,
 	ct_addr varchar2(50)
 
-)
-select * from ct_member
-
+);
 
 create table device(
 
@@ -25,14 +22,10 @@ device_num number(20),
 rent_id varchar2(20),
 ct_id varchar2(20),
 device_type char(1),
-rent_state char(1),
-broken char(1)
+rent_state char(1) default 'F',
+broken char(1) default 'F'
 
-)
-
-select * from device
-
-drop table device
+);
 
 create table contact (
 
@@ -42,30 +35,30 @@ create table contact (
  msg_text varchar2(100),
  msg_file varchar2(20)
  
-)
+);
 
 
 
-/ 렌트 pk
+-- 렌트 pk
 
 ALTER TABLE rent_member ADD CONSTRAINT PK_RENT_MEMBER PRIMARY KEY(
 rent_id
 );
 
-/ 회원 pk
+-- 회원 pk
 
 ALTER TABLE ct_member ADD CONSTRAINT PK_CT_MEMBER PRIMARY KEY(
 ct_id
 
 );
 
-/ 기기 pk
+-- 기기 pk
 ALTER TABLE device ADD CONSTRAINT PK_DEVICE PRIMARY KEY(
 device_num
 
 );
 
-/ 메시지 pk
+-- 메시지 pk
 
 ALTER TABLE contact ADD CONSTRAINT PK_CONTACT PRIMARY KEY(
 msg_num
@@ -73,7 +66,7 @@ msg_num
 );
 
 
-/ 회원 관제소 fk
+-- 회원 관제소 fk
 
 ALTER TABLE rent_member ADD CONSTRAINT FK_CT_MEMBER_TO_RENT_MEMBER FOREIGN KEY(
 ct_id
@@ -89,4 +82,21 @@ REFERENCES ct_member(
 ct_id
 )
 
-drop table contact
+
+select * from rent_member;
+
+select * from ct_member;
+
+select * from device;
+
+select * from contact;
+
+
+
+drop table rent_member;
+
+drop table ct_member;
+
+drop table device;
+
+drop table contact;
